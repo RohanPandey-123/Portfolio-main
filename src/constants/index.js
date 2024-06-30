89d53e893117fef21099dc90752064a72802c71e
RohanPandey-123 <155144256+RohanPandey-123@users.noreply.github.com>
2024-06-30 13:57:26 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -128,7 +128,7 @@ import {
       iconBg: "#E6DEDD",
       date: "Jan 2023 - Sept 2023",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Developing and maintaining web applications using Node.js and other related technologies.",
         "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
         "Participating in code reviews and providing constructive feedback to other developers.",
